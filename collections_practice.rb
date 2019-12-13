7a1c9fe81806353f0c374bd5a4b19b5634288f3c
Olesya Miller <olesyam.miller@gmail.com>
2019-12-13 00:12:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -63,11 +63,7 @@ end
 
 def add_s(array)
   array.each_with_index.map do |element, index|
-    if index != 1 
-      element << "s"   
-    else
-      element 
-    end
+    index != 1 ? element << "s" : element 
   end
 end
 
